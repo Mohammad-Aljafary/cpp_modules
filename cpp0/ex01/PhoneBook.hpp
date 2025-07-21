25b8a96ec69b92cd739e0dacf9913b3bb02c0a39
mohammad <mohammad-boom@boom>
2025-07-21 15:15:33 +0300

    enhance the ex01

--- cpp0/ex01/PhoneBook.hpp
+++ cpp0/ex01/PhoneBook.hpp
@@ -2,14 +2,18 @@
 # define PHONEBOOK_HPP
 
 # include <iostream>
+# include <cstdlib>
+# include <iomanip>
 #include "Contact.hpp"
 
 class PhoneBook {
     public:
         Contact contacts[8];
+        int numOfContact;
         void addContact();
         void searchContact();
-        void exitPhoneBook();
+        void exitPhoneBook(int flag);
+        std::string truncate(const std::string& str);
 };
 
 #endif
--- cpp0/ex01/main.cpp
+++ cpp0/ex01/main.cpp
@@ -1,49 +1,83 @@
 # include "PhoneBook.hpp"
 
-void PhoneBook::exitPhoneBook(){
-    
+void PhoneBook::exitPhoneBook(int flag){
+    if (flag)
+        std:: cout << std::endl;
+    exit(0);
 }
 
-void PhoneBook::searchContact(){
-
+std::string PhoneBook::truncate(const std::string& str) {
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";
+    return str;
 }
 
-void PhoneBook:: addContact(){
+
+void PhoneBook::searchContact() {
+    if (numOfContact == 0) {
+        std::cout << "PhoneBook is empty!" << std::endl;
+        return;
+    }
+
+    std::cout << std::setw(10) << "Index" << "|"
+              << std::setw(10) << "First Name" << "|"
+              << std::setw(10) << "Last Name" << "|"
+              << std::setw(10) << "Nickname" << std::endl;
+
+    for (int i = 0; i < numOfContact; i++) {
+        std::cout << std::setw(10) << i + 1 << "|"
+                  << std::setw(10) << truncate(contacts[i].firstName) << "|"
+                  << std::setw(10) << truncate(contacts[i].lastName) << "|"
+                  << std::setw(10) << truncate(contacts[i].nickname) << std::endl;
+    }
+}
+
+#define MAX_CONTACTS 8
+
+void PhoneBook::addContact() {
     static int index = 0;
-    
+
     std::cout << "Enter First name: ";
-    std::cin >> contacts[index].firstName;
-    
+    if (!std::getline(std::cin, contacts[index].firstName))
+        return ;
+
     std::cout << "Enter Last name: ";
-    std::cin >> contacts[index].lastName;
+    if (!std::getline(std::cin, contacts[index].lastName))
+        return ;
 
-    std:: cout << "Enter Nickname: ";
-    std:: cin >> contacts[index].nickname;
+    std::cout << "Enter Nickname: ";
+    if (!std::getline(std::cin, contacts[index].nickname))
+        return ;
 
-    std:: cout << "Enter PhoneNumber: ";
-    std:: cin >> contacts[index].phoneNumber;
+    std::cout << "Enter Phone Number: ";
+    if (!std::getline(std::cin, contacts[index].phoneNumber))
+        return ;
 
-    std:: cout << "Enter Darkest secret: ";
-    std:: cin >> contacts[index].darkestSecret;
-    index++;
-    if (index == 7)
-        index = 0;
+    std::cout << "Enter Darkest Secret: ";
+    if (!std::getline(std::cin, contacts[index].darkestSecret))
+        return ;
+    if (numOfContact < 8)
+        numOfContact++;
+
+    index = (index + 1) % 8;
 }
+
 
 int main() {
     PhoneBook phoneBook;
+    phoneBook.numOfContact = 0;
 
     std::string command;
     while (true) {
         std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
-
+        if(!std::getline(std::cin, command))
+            phoneBook.exitPhoneBook(1);
         if (command == "ADD") {
             phoneBook.addContact();
         } else if (command == "SEARCH") {
             phoneBook.searchContact();
         } else if (command == "EXIT") {
-            phoneBook.exitPhoneBook();
+            phoneBook.exitPhoneBook(0);
             break;
         } else {
             std::cout << "Unknown command. Please try again." << std::endl;
