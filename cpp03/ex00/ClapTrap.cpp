28081f146bd9b6184066f1f5ca49e668fcb953db
mohammad-aljafary <mhm0222260@ju.edu.jo>
2025-08-03 09:17:06 +0300

    add a message for each method in Clap trap class in cpp03/ex00

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,15 +1,16 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string name): name(name){
-
+    std::cout << "constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(){
-
+    std::cout << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& obj){
     *this = obj;
+    std::cout << "copy constructor called" << std::endl;
 }
 
 ClapTrap ClapTrap::operator=(const ClapTrap& obj){
@@ -20,10 +21,11 @@ ClapTrap ClapTrap::operator=(const ClapTrap& obj){
         this->hitPoints = obj.hitPoints;
         this->name = obj.name;
     }
+    std::cout << "assignment operator called" << std::endl;
     return *this;
 }
 
 ClapTrap::~ClapTrap(){
-
+    std::cout << "Destructor called" << std::endl;
 }
 
